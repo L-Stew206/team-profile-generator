8e229a5095e3d0e6aecce93121cb6ac6055b9f9b
Luke <lukeconrad.stew@gmail.com>
2022-01-22 23:30:13 -0800

    varibles

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,36 @@
 // Global Varibles 
 const inquirer = require ('inquirer');
 const fs = require ( 'fs');
-const Manager 
-const Intern
-const Enginer
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const teamArray [];
+
+function addTeamMember () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add an employee?',
+            choices: ['Yes', 'No'],
+            name: 'addTeamMember',
+        }
+    ])
+    .then(data => {
+        switch(data.addTeamMember) {
+            case 'Yes':
+                newTeamMember();
+                break;
+            case 'No':
+                fs.writeFileSync('index.html',generateTeam(team),err => err ? console.log(err):console.log('HTML Created'))
+                break;
+        }
+    })
+}
+
+
+
+
+
+    
+
+
