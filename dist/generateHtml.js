9b131233e174047fc78feae8786985adb0fd748b
Luke <lukeconrad.stew@gmail.com>
2022-04-09 23:16:20 -0700

    have bootstrap and icon links added to create cards

--- dist/generateHtml.js
+++ dist/generateHtml.js
@@ -1,11 +1,13 @@
 function generateHtml ( team ) {
-    return `<!DOCTYPE html>
+    return `
+    <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team Generator</title>
+        <title>My Generated Team of Super Stars</title>
+        
     </head>
     <body>
         <h1>Hello</h1>
